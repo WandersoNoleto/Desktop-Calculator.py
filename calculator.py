3b6cba55582dadf93df131eb0df71ec6b3ac2bd1
Wanderson Noleto <onlywanderson@gmail.com>
2022-08-17 12:34:24 -0300

    fst commit

---
+++ calculator.py
@@ -0,0 +1,85 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+from PyQt5.QtWidgets import QPushButton, QLineEdit, QSizePolicy
+
+class Calculator(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Calculator')
+        self.colorCount()
+        self.setFixedSize(320,350)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+
+        self.display = QLineEdit()
+        self.grid.addWidget(self.display, 0 ,0, 1, 5)
+        self.display.setDisabled(True)
+        self.setStyleSheet(
+            '*{background: #FFF; color: black; font-size: 25px}'
+        )
+
+        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
+        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
+        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
+        self.add_btn(QPushButton('/'), 1, 3, 1, 1)
+        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
+        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
+        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
+        self.add_btn(QPushButton('*'), 2, 3, 1, 1)
+        self.add_btn(QPushButton('1'), 3, 0, 1, 1)
+        self.add_btn(QPushButton('2'), 3, 1, 1, 1)
+        self.add_btn(QPushButton('3'), 3, 2, 1, 1)
+        self.add_btn(QPushButton('-'), 3, 3, 1, 1)
+        self.add_btn(QPushButton('0'), 4, 0, 1, 2)
+        self.add_btn(QPushButton('.'), 4, 2, 1, 1)
+        self.add_btn(QPushButton('+'), 4, 3, 1, 1)
+        
+        self.add_btn(
+            QPushButton('C'), 1, 4, 1, 1, 
+                lambda: self.display.setText(''),
+                'background: #0000ff; color: white')
+        self.add_btn(
+            QPushButton('<-'), 2, 4, 1, 1,
+                lambda: self.display.setText(
+                    self.display.text()[:-1]
+                ),
+                'background: #791515; color: white')
+        self.add_btn(
+            QPushButton('='), 3, 4, 2, 1, self.equal_method,
+                'background: #135e0d; color: white')
+
+        self.setCentralWidget(self.cw)
+
+    def add_btn(self, btn, row, col, rowspan, colspan, function=None, style=None):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
+
+        if not function:
+            btn.clicked.connect(
+                lambda: self.display.setText(
+                    self.display.text() + btn.text()
+            ))
+        else:
+            btn.clicked.connect(function)
+
+        if style:
+            btn.setStyleSheet(style)
+
+        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        
+    def equal_method(self):
+        try:
+            self.display.setText(
+                str(eval(self.display.text()))
+            )
+        except Exception as error:
+            self.display.setText('ERROR')
+
+
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    qt.exec()
